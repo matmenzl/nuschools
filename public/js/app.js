9387a8b448548a302f92405a9012eecb08ea5892
matmenzl <info@mathiasmenzl.ch>
2016-05-27 14:23:38 +0100

    set up angular router

--- public/js/app.js
+++ public/js/app.js
@@ -6,15 +6,21 @@ angular
   .module("nuschools", ['ui.router'])
   .config(Router);
 
-  Router.$inject = ["$stateProvider"];
-  function Router($stateProvider){
+  Router.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true)
     $stateProvider
       .state("main", {
-        url: "/main",
-        template: "<h2>This is working</h2>"
+        url: "/",
+        template: "<h2>Main</h2>"
+      })
+      .state("test", {
+        url: "/test",
+        template: "<h2>Test</h2>"
       });
   }
 
+urlRouterProvider.otherwise("/");
 
 
 })();
