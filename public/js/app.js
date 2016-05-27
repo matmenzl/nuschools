b3058a90fd139af8df871c22cab76b236c78b33f
matmenzl <info@mathiasmenzl.ch>
2016-05-27 14:13:40 +0100

    angular boilerplate with different states

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,17 @@
 (function(){
 
 angular
-  .module("nuschools", []);
+  .module("nuschools", ['ui.router'])
+  .config(Router);
+
+  Router.$inject = ["$stateProvider"];
+  function Router($stateProvider){
+    $stateProvider
+      .state("main", {
+        url: "/main",
+        template: "<h2>This is working</h2>"
+      });
+  }
 
 
 
